93740373a087acb919734ee428fb52c28dc099d3
Jeremy Lundy <16428663+jlundy2@users.noreply.github.com>
2021-01-13 17:44:40 -0500

    Updating jar version

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,4 +12,4 @@ if [ ! -z $RP_BINARYSTORE_MINIO_SECRETKEY_FILE ]; then
   export RP_BINARYSTORE_MINIO_SECRETKEY=$(cat $RP_BINARYSTORE_MINIO_SECRETKEY_FILE)
 fi
 
-exec java ${JAVA_OPTS} -jar service-authorization-5.3.1-exec.jar
+exec java ${JAVA_OPTS} -jar service-authorization-5.3.3-exec.jar
